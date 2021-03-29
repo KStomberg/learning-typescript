2320379cc004dde4c62a3fc5e494787f76bcb3db
KStomberg <68118089+KStomberg@users.noreply.github.com>
2021-03-29 16:32:42 -0500

    completed up to s5 p67

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,67 @@
-console.log('Working!');
+class Department {
+  //private readonly id: string;
+  // private name: string;
+  protected employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // this.name = name;
+  }
+
+  describe() {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+  }
+}
+
+class AccountingDepartment extends Department{
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting')
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name)
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+        
+    }
+
+    printReports() {
+        console.log('Reports: ' + this.reports);
+        
+    }
+}
+
+const accounting = new AccountingDepartment('d1', []);
+const IT = new ITDepartment('d2', ['Kieran', 'Jon']);
+
+accounting.addReport('Tuesday');
+accounting.addReport('Wednesday');
+
+accounting.printReports();
+
+console.log(accounting);
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Kieran');
+
+accounting.describe();
+accounting.printEmployeeInformation();
