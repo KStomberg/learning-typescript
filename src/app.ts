c615c32d9eb66cf4986e7dde1060f6e362e50913
Kieran <68118089+KStomberg@users.noreply.github.com>
2021-03-30 14:24:53 -0500

    completed s5 p67

--- src/app.ts
+++ src/app.ts
@@ -27,31 +27,51 @@ class ITDepartment extends Department {
   }
 }
 
-class AccountingDepartment extends Department{
-    constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting')
-    }
+class AccountingDepartment extends Department {
+  private lastReport: string;
 
-    addEmployee(name: string) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name)
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
     }
+    throw new Error('No report found.');
+  }
 
-    addReport(text: string) {
-        this.reports.push(text);
-        
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!');
     }
+    this.addReport(value);
+  }
 
-    printReports() {
-        console.log('Reports: ' + this.reports);
-        
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
     }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log('Reports: ' + this.reports);
+  }
 }
 
 const accounting = new AccountingDepartment('d1', []);
 const IT = new ITDepartment('d2', ['Kieran', 'Jon']);
+
+accounting.mostRecentReport = 'Year end report';
+console.log(accounting.mostRecentReport);
+console.log(accounting.mostRecentReport);
 
 accounting.addReport('Tuesday');
 accounting.addReport('Wednesday');
